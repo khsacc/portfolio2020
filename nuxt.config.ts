e0a4cfd24fbe83574771d4459eb1b986a8141716
Hiroki Kobayashi <hero.key629522@gmail.com>
2020-07-05 02:40:06 +0900

    add tags to head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,30 +7,24 @@ const nuxtConfig:Configuration = {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'server',
-  /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
   head: {
-    title: 'Hiroki Kobayashi Design Portfolio',
+    title: 'Nightie Night | Hiroki Kobayashi Design Portfolio',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      // Adobe Fonts Setting
+      { rel: 'stylesheet', href: 'https://use.typekit.net/tpq4hel.css' },
+      // Google Fonts (Noto Sans Jp) Setting
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap' }
     ]
   },
-  /*
-  ** Global CSS
-  */
   css: [
+    '@/assets/styles/fonts.scss'
   ],
-  /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
   plugins: [
   ],
   components: true,
