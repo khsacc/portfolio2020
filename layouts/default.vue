38a1a19878f75c40dc1c24c189bacdfa83241621
Hiroki Kobayashi <hero.key629522@gmail.com>
2020-07-07 22:24:54 +0900

    added header and its animation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <header-design />
+    <header-design class="header" :style="showHeader" />
     <img
       class="nuxtpage__background"
       :src="require('@/assets/img/TopPage/TopBackground.png')"
@@ -31,6 +31,11 @@ export default Vue.extend({
     backgroundObjectPosition () {
       const currentPage = pageData.find(page => page.path === this.$route.path)
       return currentPage ? currentPage.objectPosition : 8
+    },
+    showHeader () {
+      return this.$route.path !== '/'
+        ? { opacity: '1', top: '0' }
+        : { opacity: '0', top: '-20%' }
     }
   },
   watch: {
@@ -58,6 +63,12 @@ html {
 
 .disappear-on-page-transition {
   transition: opacity 0.5s
+}
+
+.header {
+  position: fixed;
+  right: 8%;
+  transition: all 1.3s cubic-bezier(.78,-0.34,.21,1.36);
 }
 
 .nuxtpage {
