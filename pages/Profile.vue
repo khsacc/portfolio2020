99a70bb1c2fff62ca9ef1e84b1461a338cf54bb0
Hiroki Kobayashi <hero.key629522@gmail.com>
2020-07-08 00:19:20 +0900

    added profile page

---
+++ pages/Profile.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <article-frame
+      page-title="Profile"
+      page-sub-title="Hiroki Kobayashi"
+    >
+      <article-section-back>
+        <p>1999年生まれ。DTPデザインが得意です。</p>
+      </article-section-back>
+    </article-frame>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import {
+  ArticleFrame,
+  ArticleSectionBack
+} from '@/components'
+
+@Component({
+  components: {
+    ArticleFrame,
+    ArticleSectionBack
+  }
+})
+export default class Profile extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
