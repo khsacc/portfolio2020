cbb7384820d4629aec7502d185a14ddb07194453
Hiroki Kobayashi <hero.key629522@gmail.com>
2020-07-07 21:05:00 +0900

    added header component

--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,11 @@
+import Header from './Header/Header.vue'
 import Footer from './Footer/Footer.vue'
 import TopToc from './TopPage/TopToc.vue'
 import TopProfile from './TopPage/TopProfile.vue'
 import TopWorks from './TopPage/TopWorks.vue'
 
 export {
+  Header,
   Footer,
   TopToc,
   TopProfile,
