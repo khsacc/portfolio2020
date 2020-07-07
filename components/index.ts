1ae47dfa5f1800087525548438c307614763027c
Hiroki Kobayashi <hero.key629522@gmail.com>
2020-07-07 22:52:28 +0900

    added new component

--- components/index.ts
+++ components/index.ts
@@ -3,11 +3,13 @@ import FooterDesign from './Footer/FooterDesign.vue'
 import TopToc from './TopPage/TopToc.vue'
 import TopProfile from './TopPage/TopProfile.vue'
 import TopWorks from './TopPage/TopWorks.vue'
+import ArticleFrame from './Article/ArticleFrame.vue'
 
 export {
   HeaderDesign,
   FooterDesign,
   TopToc,
   TopProfile,
-  TopWorks
+  TopWorks,
+  ArticleFrame
 }
