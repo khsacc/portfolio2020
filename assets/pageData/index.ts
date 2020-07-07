c2ff30fafd2dda007c59d570661d0b09c6403c60
Hiroki Kobayashi <hero.key629522@gmail.com>
2020-07-07 20:18:49 +0900

    separated pagedata

---
+++ assets/pageData/index.ts
@@ -0,0 +1,33 @@
+export type pageDatum = {
+  path: string,
+  name: string,
+  objectPosition: number,
+  shownTop: boolean
+}
+
+export const pageData: pageDatum[] = [
+  {
+    path: '/',
+    name: 'Top',
+    objectPosition: 8,
+    shownTop: false
+  },
+  {
+    path: '/Profile',
+    name: 'Profile',
+    objectPosition: 30,
+    shownTop: true
+  },
+  {
+    path: '/Works',
+    name: 'Works',
+    objectPosition: 50,
+    shownTop: true
+  },
+  {
+    path: '/Contact',
+    name: 'Contact',
+    objectPosition: 80,
+    shownTop: true
+  }
+]
