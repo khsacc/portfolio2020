f99c8de42899607733c7e5e609cacd9c8d087369
Hiroki Kobayashi <hero.key629522@gmail.com>
2020-07-09 02:01:02 +0900

    added nav in the header

--- components/Header/HeaderDesign.vue
+++ components/Header/HeaderDesign.vue
@@ -1,26 +1,90 @@
 <template>
-  <nuxt-link class="header__link" to="/">
-    <header class="header__container">
-      <span class="header__name">
-        Hiroki Kobayashi
-      </span>
-      <br>
-      <span>Design Portfolio Website</span>
-    </header>
-  </nuxt-link>
+  <header id="header">
+    <nuxt-link class="headertop__link" to="/">
+      <div class="headertop__container">
+        <span class="headertop__name">
+          Hiroki Kobayashi
+        </span>
+        <br>
+        <span>Design Portfolio</span>
+      </div>
+    </nuxt-link>
+    <div class="headernav__container ">
+      <nuxt-link v-for="page in pages" :key="page.name" :class="`headernav__link ${$route.path === page.path ? 'headernav__link--current' : ''}`" :to="page.path">
+        {{ page.name }}
+        <span :class="`headernav__underline ${$route.path === page.path ? 'headernav__underline--current' : ''}`" />
+      </nuxt-link>
+    </div>
+  </header>
 </template>
 
 <script lang="ts">
 import { Vue } from 'vue-property-decorator'
+import { pageData } from '@/assets/pageData'
 
 export default class HeaderDesign extends Vue {
-
+  public pages = pageData.filter(page => page.shownTop)
 }
 
 </script>
 
 <style lang="scss" scoped>
-.header {
+#header {
+  display: flex;
+  align-items: center;
+  background: rgb(15, 12, 22);
+  padding-right: 10px;
+
+  @include responsive(smartphone) {
+      padding-right: 0;
+    }
+}
+
+.headernav {
+  &__container {
+    color: white;
+
+    @include responsive(smartphone) {
+      display: none;
+    }
+  }
+
+  &__underline {
+    position: absolute;
+    bottom: -6px;
+    left: 0;
+    width: 0;
+    height: 1px;
+    background: #f7e0c3;
+    transition: 0.3s ease-in-out;
+
+    &--current {
+      width: 100%;
+    }
+  }
+
+  &__link {
+    margin: 0 10px;
+    color: white;
+    transition: 0.3s ease-in-out;
+    position: relative;
+
+    @include responsive(tablet) {
+      margin: 0 5px;
+    }
+
+    &:hover,
+    &--current {
+      color: #f7e0c3;
+
+      & .headernav__underline {
+        width: 100%;
+      }
+    }
+  }
+}
+
+.headertop {
   &__link {
     color: #f0c388;
     min-width: 150px;
@@ -28,7 +92,6 @@ export default class HeaderDesign extends Vue {
 
   &__container {
     padding: 15px;
-    background: rgb(15, 12, 22);
     font-family: adobe-garamond-pro;
     text-align: center;
     align-content: flex-end;
