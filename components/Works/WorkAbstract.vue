6343cf537c5e0417c402cfd5894990849d7abe42
Hiroki Kobayashi <hero.key629522@gmail.com>
2020-07-09 00:38:56 +0900

    added component to show abstract of the work

---
+++ components/Works/WorkAbstract.vue
@@ -0,0 +1,73 @@
+<template>
+  <nuxt-link :to="workData.to" class="workabstract__container">
+    <div>
+      <img class="workabstract__image" :src="workData.image">
+    </div>
+    <div class="workabstract__title">
+      {{ workData.title }}
+    </div>
+    <div class="workabstract__info">
+      {{ workData.info }}
+    </div>
+    <div>
+      <work-category v-for="category in workData.category" :key="category" :category="category" />
+    </div>
+  </nuxt-link>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import ArticleSectionBack from '@/components/Article/ArticleSectionBack.vue'
+import WorkCategory from '@/components/Works/WorkCategory.vue'
+
+@Component({
+  components: {
+    ArticleSectionBack,
+    WorkCategory
+  }
+})
+export default class WorkAbstract extends Vue {
+  @Prop({ type: Object, required: true }) public workData!: {to: string, title: string, info: string, category: string[], image: string}
+}
+</script>
+
+<style lang="scss" scoped>
+$contentMargin: 20px;
+$contentWidth: 310px;
+$contentWidth_smartphone: 280px;
+
+.workabstract {
+  &__container {
+    margin: auto auto;
+    height: 400px;
+    padding: $contentMargin;
+    display: block;
+    transition: all 0.35s ease-in-out;
+    color: white;
+    width: fit-content;
+
+    &:hover {
+      background: rgba(253, 253, 253, 0.04);
+    }
+  }
+
+  &__image {
+    width: $contentWidth;
+    display: block;
+    margin-bottom: $contentMargin;
+  }
+
+  &__title {
+    font-weight: 300;
+    font-size: 23px;
+    letter-spacing: 0.09em;
+    font-feature-settings: "palt";
+    width: $contentWidth;
+  }
+
+  &__info {
+    font-size: 14px;
+    margin: 0 0 10px 0;
+  }
+}
+</style>
--- pages/Works/index.vue
+++ pages/Works/index.vue
@@ -1,21 +1,46 @@
 <template>
-  <div class="whole__container">
-    aa
-  </div>
+  <article-frame
+    page-title="Works"
+    page-description="カードをクリックして詳細をご覧ください"
+  >
+    <div class="abstract__container">
+      <work-abstract v-for="work in worksData" :key="work.title" :work-data="work" />
+    </div>
+  </article-frame>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import {
+  ArticleFrame,
+  WorkAbstract
 } from '@/components'
 
 @Component({
   components: {
+    ArticleFrame,
+    WorkAbstract
   }
 })
-export default class Works extends Vue {}
+export default class Works extends Vue {
+  public worksData = [
+    {
+      to: '/Works/KomabaFes2019',
+      title: '第70回駒場祭「七変華」',
+      info: '2019年・共同制作',
+      category: ['Design Direction', 'DTP', 'Illustration', 'Web', 'Logo', 'Copy Writing'],
+      image: require('@/assets/img/works/KF70/abstract.jpg')
+    }
+  ]
+}
 </script>
 
 <style lang="scss" scoped>
-
+.abstract {
+  &__container {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+}
 </style>
