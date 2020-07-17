9c7bdb095d4d6b24f0809e202b3c4c78166b40f9
Hiroki Kobayashi <hero.key629522@gmail.com>
2020-07-18 00:34:02 +0900

    manage works data with params

---
+++ pages/Works/_work.vue
@@ -0,0 +1,45 @@
+<template>
+  <article-frame
+    v-if="currentWork"
+    :page-title="currentWork.title"
+    :page-sub-title="currentWork.subTitle"
+    :page-description="currentWork.description"
+    :categories="currentWork.categories"
+    :dark-back="true"
+  >
+    <article-section-back :max-width="800">
+      <p v-for="line in currentWork.detail.split('\n')" :key="line">
+        {{ line }}
+      </p>
+    </article-section-back>
+    <work-detail-list :items="currentWork.works" />
+  </article-frame>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import {
+  ArticleFrame,
+  ArticleSectionBack,
+  WorkDetailList
+} from '@/components'
+import { workData } from '@/assets/workData'
+
+@Component({
+  components: {
+    ArticleFrame,
+    ArticleSectionBack,
+    WorkDetailList
+  }
+})
+export default class EachWork extends Vue {
+  public pagePath: string = ''
+  public created () {
+    this.pagePath = this.$route.path
+  }
+
+  public get currentWork () {
+    return workData.find(work => work.to === this.pagePath)
+  }
+}
+</script>
