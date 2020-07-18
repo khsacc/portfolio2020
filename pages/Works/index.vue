3f3f9ef35dedf0ff8752f5f0ed77e3171a4a7e9b
Hiroki Kobayashi <hero.key629522@gmail.com>
2020-07-18 23:34:21 +0900

    added link to github

--- pages/Works/index.vue
+++ pages/Works/index.vue
@@ -6,6 +6,14 @@
     <div class="abstract__container">
       <work-abstract v-for="work in worksData" :key="work.abstractTitle" :work-data="work" />
     </div>
+    <div class="portfolio__github">
+      <div>
+        本サイトのソースコード（Nuxt Typescript）
+      </div>
+      <a href="https://github.com/khsacc/portfolio2020" target="_blank">
+        <img :src="require('@/assets/img/works/portfolio_github.jpg')" class="portfolio__github--img">
+      </a>
+    </div>
   </article-frame>
 </template>
 
@@ -34,6 +42,34 @@ export default class Works extends Vue {
     display: flex;
     justify-content: space-between;
     flex-wrap: wrap;
+  }
+}
+
+.portfolio {
+  &__github{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 5%;
+    text-align: center;
+
+    &--img {
+      width: 450px;
+      margin-top: 10px;
+      transition:  0.6s ease-in-out;
+
+      @include responsive(tablet) {
+        width: 380px;
+      }
+
+      @include responsive(smartphone) {
+        width: 100%;
+      }
+
+      &:hover {
+        transform: scale(1.025, 1.025);
+      }
+    }
   }
 }
 </style>
