1bdcabe251fe6c214c912302793f9ac08a025e03
Hiroki Kobayashi <hero.key629522@gmail.com>
2020-07-23 03:35:35 +0900

    set ogp

---
+++ config/ogp-meta.ts
@@ -0,0 +1,40 @@
+import { MetaPropertyProperty } from 'vue-meta'
+
+export const ogpMetaConfig:MetaPropertyProperty[] = [
+  {
+    property: 'og:type',
+    content: 'website'
+  },
+  {
+    property: 'og:title',
+    content: 'Hiroki Kobayashi Design Portfolio'
+  },
+  {
+    property: 'og: description',
+    content: 'Hiroki Kobayashi \'s design portfolio website.'
+  },
+  {
+    property: 'og:image',
+    content: 'https://khsacc-design.vercel.app/ogp.jpg'
+  },
+  {
+    property: 'og:site_name',
+    content: 'Hiroki Kobayashi Design Portfolio'
+  },
+  {
+    property: 'og:locale',
+    content: 'ja_JP'
+  },
+  {
+    property: 'twitter:card',
+    content: 'summary_large_image'
+  },
+  {
+    property: 'twitter:site',
+    content: '@khsacc'
+  },
+  {
+    property: 'twitter:creator',
+    content: '@khsacc'
+  }
+]
