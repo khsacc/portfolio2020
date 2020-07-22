6ae6c439b01e398f4921ab92338a20afa61cf0f9
Hiroki Kobayashi <hero.key629522@gmail.com>
2020-07-22 20:41:15 +0900

    test

--- pages/Works/search.vue
+++ pages/Works/search.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator'
 import { categorySearch } from '@/assets/workData'
 import {
   ArticleFrame,
